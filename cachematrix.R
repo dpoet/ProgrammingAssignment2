50928f3c09a0b3dfb294109197a0519a6f8cc49e
DPoet <Private>
2014-04-27 17:26:44 -0400

    Commiting my solution to "Programming Assignment 2". Course: R Programming (JHU, offered via Coursera / April 2014).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,84 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################################################
+# Course: R Programming - JHU, via Coursera
+#
+#  Assignment 2: "(...) write a pair of functions that cache the inverse 
+# of a matrix.".
+#
+# URL for this file (2014-APR-27) : 
+#   https://github.com/dpoet/ProgrammingAssignment2/blob/master/cachematrix.R
+#   This file contains a solution that is complete and correct.
+################################################################################
+#  The two functions: 
+#    1) makeCacheMatrix()
+#    2) cacheSolve()
+#
+#  The first function, makeCacheMatrix(), creates a special data structure for 
+# dealing with a given matrix 'x' and its inverse, 'invMatrix'.
+#
+#   The second function, cacheSolve(), takes a structure 'x' created by 
+# the makeCacheMatrix() function and returns the corresponding inverse matrix.
+################################################################################
 
-## Write a short comment describing this function
 
+# Function makeCacheMatrix()
+#   This function creates a special data structure that contains:
+#     - data: a matrix 'x' and its inverse matrix 'invMatrix';
+#     - functions: used to get/set values from/to the two matrices above.
 makeCacheMatrix <- function(x = matrix()) {
-
+    invMatrix <- NULL # Initializes the inverse matrix with a NULL value
+    
+    #  This function SETs a new value for the 'x' argument. As matrix 'x' 
+    # changes, the function also "resets" 'invMatrix' to NULL.
+    #  Note the use of the `<<-` operator, so that the correct environment 
+    # is searched and used while assigning the new values to the matrices.       
+    set <- function(newMatrix){
+        x <<- newMatrix
+        invMatrix <<- NULL
+    }
+    
+    # This function GETs the current value of the 'x' matrix
+    get <- function(){ x }
+    
+    # This function SETs a new 'invMatrix'    
+    # Note the use of the `<<-` operator, so that the correct environment 
+    #   is searched and used while assigning the new value to the matrix.        
+    setInverse <- function(newInvMatrix){
+        invMatrix <<- newInvMatrix         
+    }
+    
+    # This function GETs the current value of 'invMatrix'
+    getInverse <- function(){ invMatrix }
+    
+    #  This last statement returns a list, from which the data and functions can
+    # be accessed.
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# Function cacheSolve()
+#   This function takes a structure 'x', created by the makeCacheMatrix()
+#  function, and returns the corresponding inverse matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #  By using the function 'getInverse' one gets the current value of the 
+    # inverse matrix (or NULL, if not yet solved and stored).
+    #  If a previously solved inverse matrix is available, it will be used and 
+    # the user will be notified.
+    cachedInverse <- x$getInverse()           
+    if( ! is.null(cachedInverse) ){
+        message("Getting cached inverse matrix.")
+        result <- cachedInverse
+    } else {
+        #  If 'getInverse' returns a NULL value, one must solve the inverse 
+        # matrix and store it by using the function 'setInverse'.
+        
+        #  Used this way (with argument matrix 'M' only), the function
+        # 'solve()' will return the inverse of matrix 'M'.
+        M <- x$get()
+        result <- solve(M)
+        
+        x$setInverse(result)
+    }
+    
+    # This last statement returns the inverse matrix, based on argument 'x'.
+    result
 }
---
+++ cachematrix_Tests.R
@@ -0,0 +1,54 @@
+message("Beginning tests.")
+
+# Sources the solution file
+source("cachematrix.R")
+
+# Creates matrices A and B
+message("Matrix 'A':")
+A <- matrix(c(1:8, 1), 3, 3); A
+message("Matrix 'B':")
+B <- matrix(c(10:3,10), 3, 3); B
+
+# Creates a new structure via makeCacheMatrix and assigns it to 'm1'
+m1 <- makeCacheMatrix(A)
+message("Get stored matrix:")
+m1$get()
+
+message("Is the stored matrix identical to matrix 'A'?")
+identical(m1$get(), A)
+
+message("The inverse matrix has not yet been solved and should be equal to NULL, by now:")
+m1$getInverse()
+
+message("Let's now solve the inverse matrix...")
+cacheSolve_proto(m1)
+
+message("Done. The inverse matrix should not be NULL anymore:")
+m1$getInverse()
+
+message("If one tries to solve the inverse matrix again, she/he should get the cached inverse matrix. Let's try once:")
+cacheSolve_proto(m1)
+message("Let's try it again:")
+cacheSolve_proto(m1)
+
+message("Now what happens if we set another matrix? Say, matrix 'B' instead of 'A'.")
+message("Before, let's check the values we have by now. The inverse matrix is:")
+m1$getInverse()
+
+message("The stored matrix should be identical to matrix 'A':")
+identical(m1$get(), A)
+
+message("Now we set a new matrix: 'B'. So 'B' replaces 'A'.")
+m1$set(B)
+message("Then, the stored matrix should not be identical to matrix 'A' anymore. Is it identical to 'A'?")
+identical(m1$get(), A)
+message("Whenever we set a new matrix, the inverse matrix should be set to NULL. What's the inverse matrix by now?")
+m1$getInverse()
+
+message("OK. So let's solve the inverse matrix of the new stored matrix (which is 'B'):")
+cacheSolve(m1)
+message("OK. The inverse matrix is not NULL anymore. Let's call cacheSolve() two more times:")
+cacheSolve_proto(m1)
+cacheSolve_proto(m1)
+
+message("End of tests.")
